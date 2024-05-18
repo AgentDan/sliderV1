c880c2ace431c0db0b6e898335155205be750870
AgentSandbox <pregrada@mail.com>
2024-05-18 13:27:28 +0200

    commit1

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
-import { Canvas } from "@react-three/fiber";
-import { Leva } from "leva";
-import { Experience } from "./components/Experience";
-import { Overlay } from "./components/Overlay";
+import {Canvas} from "@react-three/fiber"
+import {Leva} from "leva"
+import {Experience} from "./components/Experience"
+import {Overlay} from "./components/Overlay"
+import Preloader from "./components/Preloader.jsx"
+import Box from "./components/Box.jsx"
+import {Suspense} from "react"
 
 function App() {
     return (
         <>
             <Leva hidden/>
-            <Overlay />
-            <Canvas shadows camera={{ position: [0, 0, 5], fov: 30 }}>
-                <color attach="background" args={["#ececec"]} />
-                <Experience />
+            <Overlay/>
+            <Canvas shadows camera={{position: [0, 0, 5], fov: 30}}>
+                <color attach="background" args={["#ececec"]}/>
+                <Suspense fallback={<Box/>}>
+                    <Experience/>
+                </Suspense>
             </Canvas>
         </>
-    );
+    )
 }
 
 export default App;
---
+++ client/src/components/consturctor/Constructor.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Constructor = () => {
+    return (
+        <div>
+            <h1>Constructor</h1>
+        </div>
+    );
+};
+
+export default Constructor;
